9b26bf265b558f298426d4e632e699c7e82f2703
MasahiroKomoto <masahiro.komoto@oasys-tech.com>
2023-12-17 11:44:41 +0900

    ソースコード解析修正

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# ToDo-CICD
+# CICDを構築する
+## larastanでソースコード解析をする
+1. `composer require --dev phpstan/phpstan`
+1. phpstanの設定ファイルを作成する
+1. `./vendor/bin/phpstan analyse -c phpstan.neon`
--- app/Models/ToDo.php
+++ app/Models/ToDo.php
@@ -4,23 +4,32 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ToDo extends Model
 {
     use HasFactory;
 
-    public function toDoDetails()
+    /**
+     * 子モデルリレーション
+     * @return HasMany
+     */
+    public function toDoDetails(): HasMany
     {
         return $this->hasMany(ToDoDetail::class);
     }
- 
-    public function delete()
+
+    /**
+     * モデルを削除する
+     * @return bool
+     */
+    public function delete(): bool
     {
         // 関連するToDoDetailsのレコードを削除する
         $this->toDoDetails()->delete();
 
         // ToDoのレコードを削除する
         return parent::delete();
-     
+
     }
 }
